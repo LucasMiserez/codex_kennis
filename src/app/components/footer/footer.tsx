375e454f959aac972f32172875273aa3a085530d
LucasMiserez <miserez.lucas@gmail.com>
2024-12-16 11:32:06 +0100

    fix/folkloristische

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="bg-white rounded-lg shadow dark:bg-gray-800">
       <div className="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-center">
         <span className="text-sm text-gray-500 text-center dark:text-gray-400">
-          Met folklorische groeten{" "}
+          Met folkloristische groeten{" "}
           <a href="https://github.com/LucasMiserez" className="hover:underline">
             Lucas Miserez
           </a>
